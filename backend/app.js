9d5e96b0a134c5bbdd5cb9fe16c1469181fc8a32
therajusah <workwithrajukumar@gmail.com>
2024-05-09 17:32:18 +0530

    updated readme

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 require("./connection/conn");
 const auth = require("./routes/auth");
+const list = require("./routes/list");
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -9,6 +11,8 @@ app.get("/", (req, res) => {
 }) 
 
 app.use("/api/v1", auth)
+app.use("/api/v2", list)
+
 
 app.listen(1000, () => {
     console.log("server Started");
---
+++ backend/routes/list.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const User = require("../modals/user");
+const List = require("../modals/list");
+
+router.post("/addTask", async (req, res) => {
+  try {
+    const { title, body, email } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const list = new List({ title, body, user: existingUser });
+      await list.save().then(() => res.status(200).json({ list }));
+      existingUser.list.push(list);
+      existingUser.save();
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+module.exports = router;
