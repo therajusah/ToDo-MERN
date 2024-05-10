d16ec9ff815ca49ea28c6003f72286052e300a95
therajusah <workwithrajukumar@gmail.com>
2024-05-11 00:27:42 +0530

    "Add route for updating task and update corresponding function in lists router"

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -2,6 +2,8 @@ const router = require("express").Router();
 const User = require("../modals/user");
 const List = require("../modals/list");
 
+//Create
+
 router.post("/addTask", async (req, res) => {
   try {
     const { title, body, email } = req.body;
@@ -16,4 +18,19 @@ router.post("/addTask", async (req, res) => {
     console.log(error);
   }
 });
+
+//update
+router.put("/updateTask/:id", async (req, res) => {
+  try {
+    const { title, body, email } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      const list = await List.findByIdAndUpdate(req.params.id, { title, body });
+      list.save().then(() =>  res.status(200).json({ message: "Task updated"}));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 module.exports = router;
