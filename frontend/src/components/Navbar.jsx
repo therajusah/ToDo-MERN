355b09e5a11f7b94a084dce2f6178d7b92f16781
therajusah <workwithrajukumar@gmail.com>
2024-05-19 08:54:49 +0530

    created navbar

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,62 @@
+import User from '../assets/user.png';
+import './Navbar.css';
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            <b>Your ToDo</b>
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  About US
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  SignUp
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  SignIn
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                  Log Out
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">
+                 <img className='img-fluid user-png' src={User} alt='user-png'></img>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
